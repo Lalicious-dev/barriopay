4e2f1ec5ad0125781cb585bbb8be7a04102c984f
manotz <guillertalacoro.az711224@gmail.com>
2025-09-17 15:49:45 -0600

    feat: add localVariables

--- backend/config/openPaymentsClient.js
+++ backend/config/openPaymentsClient.js
@@ -6,7 +6,7 @@ const privateKey = fs.readFileSync("private.key", "utf8");
 export const client = await createAuthenticatedClient({
     walletAddressUrl:"https://ilp.interledger-test.dev/ebd",
     privateKey,
-    keyId:"bdf6f295-9085-4360-91d3-716fbf36f182",
+    keyId:`${process.env.KEY_ID}`,
 })
 
 export default client;
