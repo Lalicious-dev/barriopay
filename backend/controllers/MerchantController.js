d74930484cbb065b00ea9ebe6d7642dadbe5e258
RodrigoMirandaDiaz <231163645+RodrigoMirandaDiaz@users.noreply.github.com>
2025-09-17 17:44:24 -0600

    Crear y obtener Merchant

--- backend/controllers/MerchantController.js
+++ backend/controllers/MerchantController.js
@@ -1,20 +1,33 @@
-class MerchantsController {
+import {prismaClient} from "../config/prismaClient.js";
+
+export class MerchantsController {
+    //Creamos un mercader
     static async createMerchant(req, res){
         try{
-            const {name, description, phone, walletURL} = req.body
-            const newMerchant = await prisma.merchants.create({
+            const {name, description, phone, walletURL} = req.body;
+            const newMerchant = await prismaClient.merchants.create({
                 data: {
                     name, 
                     description,
                     phone,
                     walletURL
                 }
-            })
-            res.status(201).json(newMerchant)
+            });
+            res.status(201).json(newMerchant);
         } catch (error) {
-            console.error(error)
-            res.status(500).json({message:'Erro creando merchant', error})
+            console.error(error);
+            res.status(500).json({message:'Error al crear merchant', error});
         }
     }
 
+    //Obtenemos el mercader
+    static async getMerchant(req, res){
+        try {
+            const merchants = await prismaClient.merchants.findMany();
+            res.json(merchants);
+        }catch (error){
+            res.status(500).json({message: 'Error al obtener merchants', error});
+        }
+    }
 }
+
--- backend/routes/router.js
+++ backend/routes/router.js
@@ -3,11 +3,13 @@ import {WalletController} from '../controllers/WalletController.js'
 import { walletsExists, walletsInputs } from '../middleware/wallet.middleware.js';
 import { validateErrors } from '../middleware/validateErrors.middleware.js';
 const router = express.Router();
+const MerchantsController = require("../controllers/MerchantController.js");
 
 router.get('alive', (req,res) => res.send('server alive'));
 
 // Merchants
-router.post('/merchant',()  );
+router.post('/createmerchant', MerchantsController.createMerchant);
+router.get('/getMerchant', MerchantsController.getMerchant);
 
 
 
